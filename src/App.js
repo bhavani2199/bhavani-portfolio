a9f5e157f0e76518dae3969027c4429a2a80c5c3
bhavani2199 <bhavaniambrose2199@gmail.com>
2025-07-15 02:36:11 +0800

    Update App.js and App.cs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-500">Hello Bhavani code!</h1>
+      <Header/>
     </div>
   );
 }
